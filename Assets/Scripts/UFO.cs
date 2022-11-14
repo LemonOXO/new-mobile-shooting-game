8df8b904178220d771ba47c36182d7fc07211521
LemonOXO <andy920201@gmail.com>
2022-11-15 04:29:57 +0800

    feat.修改關卡二場景，添加新敵人:幽浮(血量加倍)

---
+++ Assets/Scripts/UFO.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO : MonoBehaviour
+{
+    private float hp = 200f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet")
+        {
+            Bullet bullet = other.gameObject.GetComponent<Bullet>();
+            hp -= bullet.atk;
+
+            if (hp <= 0)
+            {
+                gameObject.SetActive(false);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
